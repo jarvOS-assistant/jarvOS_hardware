f34af6a0613e6d4c82701a9f4c95037f77ae5f15
moonstripe <kojinglick@gmail.com>
2023-11-26 21:11:46 -0800

    hello from pi

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ import speech_recognition as sr
 import time
 import requests
 import json
-
+import sys
 def main():
     mic = sr.Microphone(device_index=1)
 
     r = sr.Recognizer()
+    r.pause_threshold=5
 
     with open("device_info.json", 'r') as file:
         # Load the JSON data from the file
@@ -23,27 +24,29 @@ def main():
                     print("Listening...")
                     audio = r.listen(source, phrase_time_limit=10)
                     start_recognition = time.time()
-                    # text = r.recognize_whisper(audio, model="tiny.en")
-                    text = r.recognize_google(audio)
+                    # text = r.recognize_whisper(audio, model="tiny.en") 
+                    text = str(r.recognize_google(audio)).lower()
                     end_recognition = time.time()
 
-                    # send request to api
+                    # if hotword in text, send request to api
+                    if "jarvis" in text:
+                        command = text.replace("jarvis", "")
 
-                    print(f"You said: {text}")
-                    headers = {
-                        "Authorization": data["device_id"]
-                    }
+                        headers = {
+                            "Authorization": data["device_id"]
+                        }
 
-                    payload = json.dumps({
-                        "input": text
-                    })
+                        payload = json.dumps({
+                            "input": command
+                        })
 
-                    response = requests.post("https://jarvos.io/api/assistant", headers=headers, data=payload)
+                        response = requests.post("https://jarvos.io/api/assistant", headers=headers, data=payload)
 
-                    print(response.json())
+                        print(f"the command: {command}")
+                        print(f"returned: {response.json()}")
 
 
-                    print(f"It took {end_recognition - start_recognition} seconds")
+                        print(f"It took {end_recognition - start_recognition} seconds")
                 except sr.UnknownValueError:
                     print("Google Speech Recognition could not understand audio")
                 except sr.RequestError as e:
